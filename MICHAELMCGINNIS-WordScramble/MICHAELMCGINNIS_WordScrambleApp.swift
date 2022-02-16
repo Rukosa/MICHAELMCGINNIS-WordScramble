44ffdb6648a82a83341e9392c309e3c6f3cf16cb
Rukosa <mwmcginnis490@gmail.com>
2022-02-16 14:50:16 -0800

    Initial Commit

---
+++ MICHAELMCGINNIS-WordScramble/MICHAELMCGINNIS_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MICHAELMCGINNIS_WordScrambleApp.swift
+//  MICHAELMCGINNIS-WordScramble
+//
+//  Created by Michael Mcginnis on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct MICHAELMCGINNIS_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
